d9f9a3c95a65aa33cef4c4666026d7cd9f0bf91b
kiran <me@kiranbot.com>
2020-12-05 03:02:14 -0800

    day 5 pt 2

--- day5/day5.py
+++ day5/day5.py
@@ -3,6 +3,8 @@ import attr
 from typing import Sequence
 import math
 
+# is this a fancy bit manipulation algorithm?
+
 MAX_ROWS = 127
 MAX_COLS = 7
 
@@ -14,10 +16,19 @@ class Seat:
 
     @classmethod
     def from_seat_sequence(cls, seat_seq: str) -> Seat:
-        spec = SeatSpecifier.from_seat_sequence(seat_seq)
+        row_spec = seat_seq[:7].replace("F", "0").replace("B", "1")
+        col_spec = seat_seq[7:].replace("L", "0").replace("R", "1")
 
-        row = binary_space_partition(spec.row_spec, 0, MAX_ROWS)
-        col = binary_space_partition(spec.col_spec, 0, MAX_COLS)
+        row = int(row_spec, 2)
+        col = int(col_spec, 2)
+
+        return cls(row, col)
+
+    @classmethod
+    def from_seat_id(cls, seat_id: int) -> Seat:
+        seq = f"{seat_id:010b}"
+        row = int(seq[:7], 2)
+        col = int(seq[7:], 2)
 
         return cls(row, col)
 
@@ -26,54 +37,26 @@ class Seat:
         return (8 * self.row) + self.col
 
 
-@attr.s(auto_attribs=True)
-class SeatSpecifier:
-    row_spec: str
-    col_spec: str
+assert Seat(70, 7).seat_id == 567
+assert Seat.from_seat_id(567) == Seat(70, 7)
+assert Seat(14, 7).seat_id == 119
+assert Seat(102, 4).seat_id == 820
 
-    @classmethod
-    def from_seat_sequence(cls, seat_seq: str) -> SeatSpecifier:
-        # replace F/B/L/R with up/down instead
-        row_spec = seat_seq[:7].replace("F", "D").replace("B", "U")
-        col_spec = seat_seq[7:].replace("L", "D").replace("R", "U")
-        return cls(row_spec, col_spec)
-
-
-def binary_space_partition(sequence: str, lo: int, hi: int) -> int:
-    if lo == hi:
-        return lo
-
-    return binary_space_partition(sequence[1:], *new_range(sequence[0], lo, hi))
-
-
-def new_range(char: str, lo: int, hi: int) -> Tuple[int, int]:
-    midpoint = lo + math.ceil((hi - lo) / 2)
-    if char == "D":
-        return (lo, midpoint - 1)
-    elif char == "U":
-        return (midpoint, hi)
-    else:
-        raise ValueError(f"could not understand char {char}")
+assert Seat.from_seat_sequence("BFFFBBFRRR") == Seat(70, 7)
+assert Seat.from_seat_sequence("FFFBBBFRRR") == Seat(14, 7)
+assert Seat.from_seat_sequence("BBFFBBFRLL") == Seat(102, 4)
 
 
 with open("./input.txt") as f:
     input_lines = f.read().splitlines()
 
-max_seat_id = max(Seat.from_seat_sequence(line).seat_id for line in input_lines)
-print(max_seat_id)
+avail_seats = set(range(0, Seat(MAX_ROWS, MAX_COLS).seat_id + 1))
+taken_seats = {Seat.from_seat_sequence(line).seat_id for line in input_lines}
+potential_seat_ids = avail_seats - taken_seats
 
-# assert Seat(70, 7).seat_id == 567
-# assert Seat(14, 7).seat_id == 119
-# assert Seat(102, 4).seat_id == 820
+seats_with_occupied_neighbors = set()
+for seat in potential_seat_ids:
+    if not (seat + 1 in potential_seat_ids or seat - 1 in potential_seat_ids):
+        seats_with_occupied_neighbors.add(seat)
 
-# assert SeatSpecifier.from_seat_sequence("BFFFBBFRRR") == SeatSpecifier(
-#     row_spec="UDDDUUD", col_spec="UUU"
-# ), SeatSpecifier.from_seat_sequence("BFFFBBFRRR")
-# assert new_range("D", 0, 127) == (0, 63), new_range("D", 0, 127)
-# assert new_range("U", 0, 63) == (32, 63), new_range("U", 0, 63)
-# assert new_range("U", 32, 47) == (40, 47), new_range("U", 32, 47)
-# assert binary_space_partition("UDU", 0, MAX_COLS) == 5
-
-# assert Seat.from_seat_sequence("BFFFBBFRRR") == Seat(70, 7)
-# assert Seat.from_seat_sequence("FFFBBBFRRR") == Seat(14, 7)
-# assert Seat.from_seat_sequence("BBFFBBFRLL") == Seat(102, 4)
+print(seats_with_occupied_neighbors)
