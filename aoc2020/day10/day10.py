83d31317566e297324b51c93ccd7edac4ad0368b
kiran <me@kiranbot.com>
2020-12-10 03:32:51 -0800

    optimized combinations code

--- aoc2020/day10/day10.py
+++ aoc2020/day10/day10.py
@@ -1,6 +1,7 @@
 from typing import Dict, List, Set, Tuple
 from collections import defaultdict
 import itertools
+import math
 
 TOLERANCE = 3
 
@@ -40,21 +41,25 @@ def diff_dist_for_input_file(file_name: str) -> Dict[int, int]:
     return count_differences(chain_adapters(adapters_for_input_file(file_name)))
 
 
-assert diff_dist_for_input_file("minimal_input.txt") == {1: 7, 3: 5}
-assert diff_dist_for_input_file("medium_input.txt") == {1: 22, 3: 10}
+# assert diff_dist_for_input_file("minimal_input.txt") == {1: 7, 3: 5}
+# assert diff_dist_for_input_file("medium_input.txt") == {1: 22, 3: 10}
 
-diff_dist = diff_dist_for_input_file("input.txt")
-print(diff_dist[1] * diff_dist[3])
+# diff_dist = diff_dist_for_input_file("input.txt")
+# print(diff_dist[1] * diff_dist[3])
 
 
-def combo2(lst, n):
+def combo2(lst: List[int], n: int, prev_el=0):
     if n == 0:
         return [[]]
     l = []
     for i in range(0, len(lst)):
         m = lst[i]
+        if m - prev_el > TOLERANCE:
+            break
+
         remLst = lst[i + 1 :]
-        for p in combo2(remLst, n - 1):
+
+        for p in combo2(remLst, n - 1, m):
             l.append([m] + p)
     return l
 
@@ -65,12 +70,12 @@ def valid_combinations(adapters: List[int]) -> int:
     sorted_adapters = sorted(adapters)
     target = sorted_adapters[-1] + 3
 
-    for r in range(1, len(sorted_adapters) + 1):
+    min_steps = math.ceil(target / 3)
+
+    for r in range(min_steps, len(sorted_adapters) + 1):
         print(f"Trying candidates of len {r} / {len(sorted_adapters)}")
         for option in combo2(sorted_adapters, r):
-            candidate = [0, *option, target]
-            if validate_differences(candidate):
-                print(candidate)
+            if target - option[-1] <= TOLERANCE:
                 valid_options += 1
 
         print(f"Found {valid_options} candidates so far.")
@@ -80,6 +85,6 @@ def valid_combinations(adapters: List[int]) -> int:
 
 # valid_combinations([5, 8, 10])
 assert valid_combinations(adapters_for_input_file("minimal_input.txt")) == 8
-# assert valid_combinations(adapters_for_input_file("medium_input.txt")) == 19208
+assert valid_combinations(adapters_for_input_file("medium_input.txt")) == 19208
 
 # print(valid_combinations(adapters_for_input_file("input.txt")))
